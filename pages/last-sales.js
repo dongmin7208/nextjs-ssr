b9c25b8940e3c3451c6adb920e0b9ce60f3b80d4
dongmin7208 <sayg3o3d@gmail.com>
2023-01-14 16:13:20 +0900

    ADD:last-sales

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,31 +1,32 @@
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+export default function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
+  // const [isLoading, setIsLoading] = useState(false);
 
-export default function LastSalesPage() {
-  const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  const { data, error } = useSWR(
+    'https://nextjs-course-b532c-default-rtdb.firebaseio.com/sales.json'
+  );
   useEffect(() => {
-    setIsLoading(true);
-    fetch('https://nextjs-course-b532c-default-rtdb.firebaseio.com/sales.json')
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+    if (data) {
+      const transformedSales = [];
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
-  if (isLoading) {
-    return <p>Loading...</p>;
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+    // setIsLoading(false);
+  }, [data]);
+  if (error) {
+    return <p>Failed to load.</p>;
   }
-  if (!sales) {
-    return <p>No data yet</p>;
+  if (!data && !sales) {
+    return <p>Loading...</p>;
   }
   return (
     <ul>
@@ -36,4 +37,20 @@ export default function LastSalesPage() {
       ))}
     </ul>
   );
+}
+export async function getStaticProps() {
+  const response = await fetch(
+    'https://nextjs-course-b532c-default-rtdb.firebaseio.com/sales.json'
+  );
+  const data = await response.json();
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+  return { props: { sales: transformedSales } };
 }
